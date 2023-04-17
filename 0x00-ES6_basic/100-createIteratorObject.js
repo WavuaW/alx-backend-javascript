5fff448192190a6d46955e03c1ea0c357b475504
WavuaW <wavuamuroka@gmail.com>
2023-04-17 22:34:05 +0300

    advanced

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+    const thearray = [];
+    for (const item of Object.values(report.allEmployees)) {
+      thearray.push(...item);
+    }
+  
+    return thearray;
+  }
