97f5ce5260b59cea87ccc86ed4648312fe782165
WavuaW <wavuamuroka@gmail.com>
2023-04-19 22:32:46 +0300

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(filename),
-  ])
-    .then((responses) => {
-      responses.map((response) => ({
-        status: response.status,
-        value:
-          response.status === 'fulfilled'
-            ? response.status
-            : String(response.reason),
-      }));
-    })
-    .catch(() => {
-      console.error(Error());
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
     });
+    return result;
+  });
 }
