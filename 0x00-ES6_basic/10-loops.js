b8b5ef6a81fccd9ad0ca99cb65cb779a2e9ae187
WavuaW <wavuamuroka@gmail.com>
2023-04-19 14:09:38 +0300

    fixing lint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const thearray = [];
-    for (const item of array) {
-      thearray.push(appendString + item);
-    }
-  
-    return thearray;
+  const thearray = [];
+  for (const item of array) {
+    thearray.push(appendString + item);
   }
+
+  return thearray;
+}
--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,8 @@
 export default function createIteratorObject(report) {
-    const thearray = [];
-    for (const item of Object.values(report.allEmployees)) {
-      thearray.push(...item);
-    }
-  
-    return thearray;
+  const thearray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    thearray.push(...item);
   }
+
+  return thearray;
+}
