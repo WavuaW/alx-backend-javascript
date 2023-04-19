f1105ecb71089e40c7fc21b4da105dcff6514617
WavuaW <wavuamuroka@gmail.com>
2023-04-19 21:15:46 +0300

    lint

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
 export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-        if(true){
-            resolve()
-        }
-        else{
-            reject()
-        }
-    });
+  return new Promise((resolve) => {
+    resolve();
+  });
 }
