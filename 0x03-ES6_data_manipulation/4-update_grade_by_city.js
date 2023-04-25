d5bc015390d14e31c19e4266a2d5dd6b94d72b28
WavuaW <wavuamuroka@gmail.com>
2023-04-25 19:07:30 +0300

    using filtter and map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,20 +1,16 @@
-export default function updateStudentGradeByCity(list, city, newGrade) {
-  return list
-    .filter((obj) => obj.location === city)
+export default function updateStudentGradeByCity(newArray, city, newGrade) {
+  return newArray
+    .filter((OurObj) => OurObj.location === city)
     .map((student) => {
-    //   console.log(newGrade);
       newGrade.map((studentGrade) => {
         if (studentGrade.studentId === student.id) {
-          // eslint-disable-next-line no-param-reassign
           student.grade = studentGrade.grade;
         }
-
         if (!student.hasOwnProperty('grade')) {
           student.grade = 'N/A';
         }
         return student;
       });
-
       return student;
     });
 }
